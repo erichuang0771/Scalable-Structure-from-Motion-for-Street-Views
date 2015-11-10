5760cfa34be2685180df5975f8f033c792be207f
eric huang <huan0771@umn.edu>
2015-11-10 09:22:53 -0500

    ,,,,,,

--- test/addNewView.m
+++ test/addNewView.m
@@ -1,6 +1,7 @@
-function [ pc, newfeatureTable ] = addNewView( ims, featureTable, M)
+function [ pc, newfeatureTable ] = addNewView( ims,ims2, featureTable, M,K1,K2)
 %UNTITLED2 Summary of this function goes here
-%   featureTable SIFTdes|3D corrd|P matrix
+%   featureTable SIFTdes|3D corrd|2D corrd|P matrix|K matrix
+%
 % SIFTdes = 128 * N
 
       [f,d] = vl_sift(single(rgb2gray(ims)));
@@ -8,12 +9,6 @@ function [ pc, newfeatureTable ] = addNewView( ims, featureTable, M)
       xx = [f(1,matches(1,:))' f(2,matches(1,:))', ones(size(matches(1,:)))'];
       XX = featureTable(matches(2,:),129:129+2);
       [R t]= RPnP(XX,xx);
-      P2 = featureTable()
-      [ F, P1_inlier, P2_inlier ] = ransacF( P1(:,1:2), P2(:,1:2), M);
-
-     
-    
-
 
 end
 
