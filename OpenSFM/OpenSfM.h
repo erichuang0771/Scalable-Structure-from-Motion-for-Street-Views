6e3404567bdec8b115e5dc0ddc69bac2fac9faf2
erichuang0771 <ihuangyujia@me.com>
2015-11-25 02:27:18 -0500

    need to solve how to plot epoliar lines??

--- OpenSFM/OpenSfM.h
+++ OpenSFM/OpenSfM.h
@@ -46,14 +46,14 @@ int run(std::string dir_images);
 private:
 
 int loadParas();
-/*
+/* Yilin Yang
 	load intrinsc K and all thresholds....from configure txt file	
 	input: unsure
 	output: 1 if succcess
 		   0 if error
  */
 int multiViewTriangulation(arma::umat& index , cv::Mat& ims);
-/*
+/* Yilin Yang
 	update 3d points in feature table that viewed by at least three camseras
 	input: index, the entry that you want to update
 			ims: image, in order to get color info
@@ -61,7 +61,7 @@ int multiViewTriangulation(arma::umat& index , cv::Mat& ims);
 		   0 if error
  */
 last_frame* updateStruture(cv::Mat& ims, last_frame& last_frame );
-/*
+/*  Eric Huang & Yilin Yang
 	write new data into all tables and compute camera R and T
 	input: ims: new image that going to estimates
 		   last_frame: the struct that contains all info needed in updating
@@ -69,7 +69,7 @@ last_frame* updateStruture(cv::Mat& ims, last_frame& last_frame );
 		   
  */
 last_frame* initalTwoViewRecon(cv::Mat& imA, cv::Mat& imB);
-/*
+/*  Eric Huang
 	init all internal tables and create two view gemotry
 	input: imA imB: new image that going to estimates
 		   
