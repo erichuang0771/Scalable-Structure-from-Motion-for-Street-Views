13d6e8750d3784d2c9f005990caaecac822c7dda
Allen <allenyangyl@gmail.com>
2015-12-03 00:24:57 -0500

    multi triangulation

--- OpenSFM2/utils.cpp
+++ OpenSFM2/utils.cpp
@@ -17,7 +17,7 @@ bool OpenSfM::loadParas(std::string dir){
 			getline(paraFile, line);
 		elem[i] = stof(line);
 	}
-	this -> intrinsc_K 
+	this -> intrinsc_K
 		<< elem[0] << elem[1] << elem[2] << arma::endr
         	<< elem[3] << elem[4] << elem[5] << arma::endr
         	<< elem[6] << elem[7] << elem[8];
@@ -36,13 +36,48 @@ bool OpenSfM::loadParas(std::string dir){
 		cv::Mat img = cv::imread(line + ss.str() + ".png",  CV_LOAD_IMAGE_COLOR);
 		this -> images.push_back(img);
 	}
-	
+
 	return 1;
 };
 
 int OpenSfM::multiViewTriangulation(arma::umat& index , cv::Mat& ims){
-	cout << "index: " << index << endl;
-	cout << "Zi" << this -> Z_i << endl;
-	cout << "Zj" << this -> Z_j << endl;
+	vector<arma::fmat*>* camProjTable = this -> camProjTable;
+	vector<arma::fmat*>* featureCell = this -> featureCell;
+	vector<arma::fmat*>* cameraPose = this -> cameraPose;
+	vector<unsigned>* Z_i = this -> Z_i;
+	vector<unsigned>* Z_j = this -> Z_j;
+
+	/*cout << "index: " << index << endl;
+	cout << "Zi ";
+	for(int i = 0; i < (*(this -> Z_i)).size(); i++)
+		cout << (*(this -> Z_i))[i] << ' ';
+	cout << "\nZj ";
+	for(int i = 0; i < (*(this -> Z_j)).size(); i++)
+		cout << (*(this -> Z_j))[i] << ' ';*/
+
+	int index_num = index.n_rows;
+	for(int i = 0; i < index_num; i++){
+		int idx = index(i, 0);
+		// 2D point in camera idx
+		arma::fmat pts = *(*featureCell)[idx];
+		//std::cout << "featureCell" << pts << std::endl;
+
+		// get all the (Z_i, Z_j, Z_v) from camera idx
+		vector<int> Z_index;
+		for(int j = 0; j < (*Z_j).size(); j++)
+			if((*Z_j)[j] == idx && (j == 0 || (*Z_j)[j - 1] != (*Z_j)[j]))
+				Z_index.push_back((*Z_i)[j]);
+
+		// get the matrix for SVD decomposition
+		int n = Z_index.size();
+		arma::fmat A(2 * n, 4);
+		for(int j = 0; j < Z_index.size(); j++){
+			std::cout << "Z_index" << Z_index[j] << std::endl;
+			std::cout << "Proj" << (*(*camProjTable)[Z_index[j]]) << std::endl;
+			//A(j, 0) = (*(*camProjTable)[idx])//(2, 0) * pts(0, 0);
+		}
+	}
+
+
 	return 1;
 };
