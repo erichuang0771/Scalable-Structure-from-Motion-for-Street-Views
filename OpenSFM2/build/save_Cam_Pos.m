51ec5c0ed8bddb5673f8daf6b22a0c61d4a9f3ef
erichuang0771 <ihuangyujia@me.com>
2015-12-04 16:07:52 -0500

    asdasd

---
+++ OpenSFM2/build/save_Cam_Pos.m
@@ -0,0 +1,14 @@
+function [] = save_Cam_Pos(camProjTable)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+    camera_center = zeros(size(camProjTable,3),6);
+    for i = 1:size(camProjTable,3)
+        R = camProjTable(1:3,1:3,i);
+        T = camProjTable(:,4,i);
+        camera_center(i,:) = [(-inv(R)*T)', [128,128,0]];
+    end
+      camera_center(end,4:6) = [200,0,0];
+    save_ply('Cams.ply',camera_center);
+end
+
+
